3b2665f8e732dbde4c78c64819b039621221e702
dds <mauri.dupo@yahoo.com.ar>
2018-04-24 14:52:14 -0300

    Actualizacion de App y json

--- src/main/java/TP_DDS_SGE/TP/App.java
+++ src/main/java/TP_DDS_SGE/TP/App.java
@@ -3,20 +3,27 @@ package TP_DDS_SGE.TP;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class App 
-{
-    public static void main( String[] args ) throws FileNotFoundException{
-    	String path = "json.txt";
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-
-        Gson gson = new Gson();
-        Object json = gson.fromJson(bufferedReader, Object.class);
-
-        System.out.println(json.getClass());
-        System.out.println(json.toString());
-    }
+public class App {
+	
+	public static BufferedReader leerArchivo() throws FileNotFoundException {
+    	
+    	BufferedReader bufferedReader = new BufferedReader(new FileReader("jsonList.txt"));
+    	
+    	return bufferedReader;
+	}
+        
+	public static List<Cliente> armarLista() throws FileNotFoundException{
+		Gson gson = new Gson();
+        Type tipoListaclientesJSON = new TypeToken<List<Cliente>>(){}.getType();
+        List<Cliente> usuarios = gson.fromJson(leerArchivo(), tipoListaclientesJSON);
+        
+        return usuarios;
+	}
 }
 
