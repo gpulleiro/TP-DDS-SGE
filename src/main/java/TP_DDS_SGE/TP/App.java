6acae3c426b2364f7b48f358852f6a0296dc3318
Gaston Pulleiro <gpulleiro.utn@gmail.com>
2018-04-25 16:08:02 -0300

    Se arreglo el problema del JSON

--- src/main/java/TP_DDS_SGE/TP/App.java
+++ src/main/java/TP_DDS_SGE/TP/App.java
@@ -1,29 +1,11 @@
 package TP_DDS_SGE.TP;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class App {
 	
-	public static BufferedReader leerArchivo() throws FileNotFoundException {
-    	
-    	BufferedReader bufferedReader = new BufferedReader(new FileReader("jsonList.txt"));
-    	
-    	return bufferedReader;
-	}
-        
-	public static List<Cliente> armarLista() throws FileNotFoundException{
-		Gson gson = new Gson();
-        Type tipoListaclientesJSON = new TypeToken<List<Cliente>>(){}.getType();
-        List<Cliente> usuarios = gson.fromJson(leerArchivo(), tipoListaclientesJSON);
-        
-        return usuarios;
+	public static void main( String[] args ) throws FileNotFoundException{
+		
+		RepositorioUsuarios.importarJSON("jsonList.txt");
 	}
 }
-
--- src/main/java/TP_DDS_SGE/TP/RepositorioUsuarios.java
+++ src/main/java/TP_DDS_SGE/TP/RepositorioUsuarios.java
@@ -1,43 +1,62 @@
 package TP_DDS_SGE.TP;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.Iterator;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class RepositorioUsuarios {
 	
-	private static List<Cliente> clientes;
-	
-	public static void inicializarRepositorio() throws FileNotFoundException{
-		
-		
-		clientes = App.armarLista();
-		
-		
+	private  ArrayList<Cliente> clientes;
+
+	//constructor
+	public RepositorioUsuarios(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
 	}
-	
-	
-	public static boolean existeUsuarioDeNombre(String nombre) {
-		return clientes.stream().anyMatch(cliente -> cliente.getNombre().equals(nombre));
-	}
-	
-	/*TODO
-	public static Cliente getClienteDeNombre(String nombre) {
-		
-	
-	}
-	*/
-	
-	public static List<Cliente> getClientes(){
-		
+
+	//Getter and Setters
+	public ArrayList<Cliente> getClientes() {
 		return clientes;
 	}
-	
-	public static void setClientes(List<Cliente> clientesNuevos){
-		
-		clientes = clientesNuevos;
+
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
 	}
 	
-
+	// metodos
+	
+	public static void importarJSON (String json) throws FileNotFoundException {
+		
+		Type tipoListaCliente = new TypeToken<ArrayList<Cliente>>(){}.getType();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(json));
+		Gson gson = new Gson();
+		ArrayList<Cliente> listaClientes = gson.fromJson(bufferedReader, tipoListaCliente);
+		
+		//para comprobar que importo correctamente
+		Cliente cli;
+		Dispositivo dis;
+		
+		Iterator itercli = listaClientes.iterator();
+		
+		while(itercli.hasNext()){
+			cli = (Cliente)itercli.next();
+			
+			System.out.println("nombre: " + cli.getNombre() +" -apellido: " + cli.getApellido() +" -domicilio: " + cli.getDomicilio() +" -usuario: " + cli.getUsuario()+ " -contrasenia: " + cli.getContrasenia() + "\n");
+			
+			Iterator iterdis = cli.getDispositivos().iterator();
+			
+			while(iterdis.hasNext()){
+				dis = (Dispositivo)iterdis.next();
+				
+				System.out.println("nombre dis: "+dis.getNombre()+ " -consumo dis: "+ dis.getConsumo() + " -estado dis: " + dis.isEstado() + "\n");
+				
+			}
+		}
+	}
+	
 }
