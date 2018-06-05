c8909a0a3d57cd1c2ffbec0533c952d67c36d981
Gaston Pulleiro <gpulleiro.utn@gmail.com>
2018-06-05 00:23:30 -0300

    para que los chicos realicen las pruebas necesarias

--- src/main/java/TP_DDS_SGE/TP/App.java
+++ src/main/java/TP_DDS_SGE/TP/App.java
@@ -1,28 +1,30 @@
-package TP_DDS_SGE.TP;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public class App {
-	
-	public static void main( String[] args ) throws FileNotFoundException{
-		
-		try {
-			Repositorio.importarClientes("clientes.txt");
-			Repositorio.importarLog("log.txt");
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		
-		// prueba generar log
-		Cliente pepe = new Cliente();
-		
-	//	Dispositivo licuadora = new Dispositivo("licuadora",20,new Estandar(3));
-		Dispositivo licuadora = new Dispositivo("licuadora",20,new Inteligente("encendido"));
-		
+//package TP_DDS_SGE.TP;
+//
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.util.Iterator;
+//
+//public class App {
+//	
+//	public static void main( String[] args ) throws FileNotFoundException{
+//		
+//		try {
+//			//Repositorio.importarDispositivos("dispositivos.txt");
+//			Repositorio.importarClientes("clientes.txt");
+//			Repositorio.importarLog("log.txt");
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+//		
+//		
+//		// prueba generar log
+//		Cliente pepe = new Cliente();
+//		
+//		Dispositivo licuadora = new Dispositivo("licuadora",20,new Estandar(3));
+//	//	Dispositivo licuadora = new Dispositivo("licuadora",20,new Inteligente("encendido"));
+//		
 //		pepe.convertirDispositivo(licuadora);
-
+//
 //		try {
 //			licuadora.encender();
 //			licuadora.ahorro();
@@ -31,10 +33,25 @@ public class App {
 //		} catch (IOException e) {
 //			e.printStackTrace();
 //		}
-		
-//		double consumo = licuadora.consumoUltimasHoras(8);
-		double consumo = licuadora.consumoPeriodo("29/05/2018 02:30:00","29/05/2018 11:00:00");
-		
-		System.out.println("el consumo fue: " + consumo +" Kw/H");
-	}
-}
+//		
+////		double consumo = licuadora.consumoUltimasHoras(8);
+//		double consumo = licuadora.consumoPeriodo("04/06/2018 23:59:00","15/06/2018 00:00:00");
+//		
+//		System.out.println("el consumo fue: " + consumo +" Kw/H");
+//		
+//		//para comprobar que importo correctamente
+//		
+////		Log log;
+////		
+////		
+////		Iterator iLog = Repositorio.log.iterator();
+////		
+////		while(iLog.hasNext()){
+////			
+////			log = (Log)iLog.next();
+////			
+////			System.out.println(" fecha: " + log.getFecha() +" -nombre: " + log.getNombre() +" -estado: " + log.getEstado() + "\n");
+////			
+////		}
+//	}
+//}
--- src/main/java/TP_DDS_SGE/TP/PruebaSensores.java
+++ src/main/java/TP_DDS_SGE/TP/PruebaSensores.java
@@ -7,19 +7,35 @@ public class PruebaSensores {
 
 	public static void main(String[] args) throws IOException {
 		
-		Dispositivo luz = new Dispositivo("luz",20,new Inteligente("encendido"));
+		Repositorio.importarLog("log.txt");
 		
-
+		
+		Dispositivo luz = new Dispositivo("luz",20,new Inteligente("encendido"));
+		Dispositivo luz1 = new Dispositivo("luz1",20,new Inteligente("encendido"));
+		Dispositivo luz2 = new Dispositivo("luz2",20,new Inteligente("encendido"));
+		
+		
+		
+//		try {
+//			luz.encender();
+//			luz.ahorro();
+//			luz.apagar();
+//			luz.apagar();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
 		SensorDeMovimiento sensorLuces = new SensorDeMovimiento();
 		ReglaDeMovimiento sinMovimientoApagar = new ReglaDeMovimiento();
 		ActuadorApagar apagar = new ActuadorApagar();
 		
 		sensorLuces.agregarDispositivo(luz);
+		sensorLuces.agregarDispositivo(luz1);
 		sinMovimientoApagar.agregarSensor(sensorLuces); 
 		sinMovimientoApagar.setActuador(apagar);
 		
 		sensorLuces.realizarMedicion();
-//		
-		//luz.getTipo().imprimirEstado();
+		
+		System.out.println(luz.getEstado()+" "+luz1.getEstado()+" "+luz2.getEstado());
 	}
 }
