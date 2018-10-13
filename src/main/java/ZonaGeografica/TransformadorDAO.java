358e1f0cd7f72442942440faef8a5697fc33d8c8
mauricio <mauri.dupo@yahoo.com.ar>
2018-10-12 21:43:53 -0300

    agrego TransformadorDAO

---
+++ src/main/java/ZonaGeografica/TransformadorDAO.java
@@ -0,0 +1,59 @@
+package ZonaGeografica;
+
+
+import java.util.List;
+
+
+
+import javax.persistence.EntityManager;
+
+import javax.persistence.EntityTransaction;
+
+
+
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
+
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+
+
+
+import Repositorio.Repositorio;
+
+
+public class TransformadorDAO implements WithGlobalEntityManager{
+
+	EntityManager entityManager = PerThreadEntityManagers.getEntityManager();
+	EntityTransaction transaccion = entityManager.getTransaction();
+
+	public void registrarTransformador(Transformador transformador) {
+
+		entityManager().persist(transformador);
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Transformador> listarTransformadores(){
+
+		return entityManager().createQuery("FROM Transformador").getResultList();
+
+	}
+
+	
+	public void cargaInicial() {
+
+		Repositorio repo = Repositorio.getInstance();
+		
+		for(Transformador trafo : repo.getTransformadores()){
+	
+			entityManager().persist(trafo);
+			
+		}
+//		for(Zona zona: repo.getZonas()){
+//
+//			entityManager().persist(zona);
+//
+//		}
+	
+	}
+
+}
